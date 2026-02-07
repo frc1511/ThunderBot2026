77c55a217fb399bc65d9d5fb9c56860278121dee
SGOTI2 <149632986+SGOTI2@users.noreply.github.com>
2026-02-07 01:39:06 -0500

    Apply non-null init value to isDisabled in thunderSubsystem

--- src/main/java/frc/util/ThunderSubsystem.java
+++ src/main/java/frc/util/ThunderSubsystem.java
@@ -3,7 +3,7 @@ package frc.util;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ThunderSubsystem extends SubsystemBase {
-    private boolean m_isDisabled;
+    private boolean m_isDisabled = false;
 
     public boolean getDisabled() {
         return m_isDisabled;
