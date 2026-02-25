986d5362f354e0b643da9332f2ddc60df6fc43dc
SGOTI2 <149632986+SGOTI2@users.noreply.github.com>
2026-02-24 21:52:37 -0500

    Require robot build success to deploy

---
+++ src/test/java/BuildTest.java
@@ -0,0 +1,51 @@
+import edu.wpi.first.hal.HAL;
+import frc.robot.Robot;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BuildTest {
+  static Robot robot = null;
+
+  @BeforeEach
+  void robotBuilds() {
+    assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
+    if (robot == null) {
+      robot = new Robot();
+      assertNotNull(robot);
+    }
+  }
+
+  @Test
+  void nonNullSubsystems() {
+    assertNotNull(robot);
+    assertNotNull(robot.drivetrain);
+    assertNotNull(robot.shooter);
+    assertNotNull(robot.hood);
+    assertNotNull(robot.turret);
+    assertNotNull(robot.spindexer);
+    assertNotNull(robot.intake);
+    assertNotNull(robot.pivot);
+    assertNotNull(robot.hang);
+  }
+
+  @Test
+  void init() {
+    assertNotNull(robot);
+    robot.robotInit();
+    robot.disabledInit();
+    robot.autonomousInit();
+    robot.teleopInit();
+  }
+  
+  @Test
+  void periodic() {
+    assertNotNull(robot);
+    robot.robotPeriodic();
+    robot.disabledPeriodic();
+    robot.autonomousPeriodic();
+    robot.teleopPeriodic();
+  }
+}
