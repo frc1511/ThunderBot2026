20d4e62edc265e71c743f9e317b3c6a35d07f690
EHook15 <kirby8151511@gmail.com>
2026-04-02 19:46:27 -0400

    testing the new intake
    make sure the motors are inverted if this gets changed and you test the intake!

--- src/main/java/frc/robot/subsytem/Moving.java
+++ src/main/java/frc/robot/subsytem/Moving.java
@@ -9,16 +9,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.util.CommandBuilder;
 
 public class Moving extends SubsystemBase {
-    private TalonFX m_motor;
+    private TalonFX m_rmotor;
+    private TalonFX m_lmotor;
     private AnalogInput m_potentiomiter;
 
     public Moving() {
-        m_motor = new TalonFX(42);
+        m_rmotor = new TalonFX(1);
+        m_lmotor = new TalonFX(0);
         m_potentiomiter = new AnalogInput(0);
     }
 
     private double getRunSpeed() {
-        return m_potentiomiter.getVoltage() / 5;
+        // return m_potentiomiter.getVoltage() / 5;
+        return .50;
     }
 
     public void periodic() {
@@ -28,21 +31,24 @@ public class Moving extends SubsystemBase {
     public Command runForward() {
         return new CommandBuilder(this)
             .onExecute(() -> {
-                m_motor.set(getRunSpeed());
+                m_rmotor.set(getRunSpeed());
+                m_lmotor.set(-getRunSpeed());
             });
     }
 
     public Command runBackward() {
         return new CommandBuilder(this)
             .onExecute(() -> {
-                m_motor.set(-getRunSpeed());
+                m_rmotor.set(-getRunSpeed());
+                m_lmotor.set(getRunSpeed());
             });
     }
 
     public Command stop() {
         return new CommandBuilder(this)
             .onExecute(() -> {
-                m_motor.set(0);
+                m_rmotor.set(0);
+                m_lmotor.set(0);
             });
     }
 }
